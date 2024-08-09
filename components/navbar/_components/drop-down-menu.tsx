86997f4416a47b01aa3cdfc8855ca0ecbb5ac312
ayodeji akinola <ayodejiakinola007@gmail.com>
2024-08-09 04:56:37 +0100

    b

--- components/navbar/_components/drop-down-menu.tsx
+++ components/navbar/_components/drop-down-menu.tsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+  } from "@/components/ui/accordion"
+  import {Settings, UserPlus, DollarSign, Headphones } from "lucide-react"
+import Link from "next/link"
+  
 interface DropDownMenuProps {
     onClose: () => void
 }
@@ -9,7 +17,21 @@ const DropDownMenu: React.FC<DropDownMenuProps>= ({onClose}) => {
   
     }
   return (
-    <div className='w-screen h-screen px-4 items-center justify-center absolute right-0 xl:hidden'> DropDownMenu</div>
+    <div className='mt-20 w-screen h-screen px-4 items-center justify-center absolute right-0 xl:hidden'> 
+    <Accordion defaultValue={"item-1"} className="pl-2" type="single" collapsible>
+    <AccordionItem className="mt-6 bottom-b" value="item-1">
+        <AccordionTrigger>Use case</AccordionTrigger>
+        <AccordionContent defaultValue={"item-1"} className="space-y-2">
+            <Link href={"#"} className="flex" onClick={handleLinkClick}>
+            <div className="">
+                <Settings className="h-6 w-6 mr-2 text-custom-blue"/>
+            </div>
+            <div className="">Sales</div>
+            </Link>
+        </AccordionContent>
+    </AccordionItem>
+    </Accordion>
+    </div>
   )
 }
 
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,17 +1,17 @@
-import ActionButtons from "./_components/action-buttons"
-import Logo from "./_components/logo"
-import { Menu } from "./_components/menu"
-
+import ActionButtons from "./_components/action-buttons";
+import Logo from "./_components/logo";
+import {Menu} from "./_components/menu";
 
 const Navbar = () => {
-  const navBarClasses = `container flex items-center justify-between space-x-10 bg-white h-14 sticky top-0 z-50 border-b border-gray-200`
+  const navBarClasses = `container flex items-center justify-between space-x-4 sm:space-x-6 md:space-x-8 lg:space-x-10 bg-white h-14 sticky top-0 z-50 border-b border-gray-200`;
+
   return (
     <div className={navBarClasses}>
-        <Logo/>
-        <Menu/>
-        <ActionButtons/>
+      <Logo />
+      <Menu />
+      <ActionButtons />
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
